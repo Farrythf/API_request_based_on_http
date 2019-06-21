a7b16649b0e3365dd7a66f44902b909c88600288
Farrythf <hongfeit@usc.edu>
2019-06-21 16:52:23 -0700

    0621

---
+++ test.js
@@ -0,0 +1,270 @@
+setInterval(function(){
+	
+	var time = new Date();
+
+	var s_min = time.getMinutes();
+	var s_hour = time.getHours();
+	var s_year = time.getFullYear();
+	var s_month = time.getMonth() + 1;
+	var s_day = time.getDate();
+
+	s_min = parseInt(s_min / 15);
+	if(s_min == 0){
+		s_min = 45;
+		s_hour = s_hour - 1;
+		if(s_hour == -1){
+			s_hour = 23;
+			s_day = s_day - 1;
+			if(s_day == 0){
+				switch(s_month){
+						case 12:
+							s_day = 30;
+							break;
+						case 11:
+							s_day = 31;
+							break;
+						case 10:
+							s_day = 30;
+							break;
+						case 9:
+							s_day = 31;
+							break;
+						case 8:
+							s_day = 31;
+							break;
+						case 7:
+							s_day = 30;
+							break;
+						case 6:
+							s_day = 31;
+							break;
+						case 5:
+							s_day = 30;
+							break;
+						case 4:
+							s_day = 31;
+							break;
+						case 3:
+							if (s_year % 100 == 0){
+								if (s_year % 400 == 0)
+								{
+									s_day = 29;
+								}
+								else
+								{
+									s_day = 28;
+								}
+							}
+							else{
+								if (s_year % 4 == 0)
+								{
+									s_day = 29;
+								}
+								else
+								{
+									s_day = 28;
+								}
+							}
+							break;
+						case 2:
+							s_day = 31;
+							break;
+						case 1:
+							s_day = 31;
+							break;
+						
+				}
+				s_month = s_month - 1;
+				if(s_month == 0){
+					s_month = 12;
+					s_year = s_year - 1;
+				}
+			}
+		}
+	}
+	else{
+		s_min = (s_min-1) * 15;
+	}
+
+	var e_min = s_min+15;
+	var e_hour = s_hour;
+	var e_year = s_year;
+	var e_month = s_month;
+	var e_day = s_day;
+
+	if(e_min == 60){
+		e_min = 0;
+		e_hour = e_hour + 1;
+		if(e_hour == 24){
+			e_hour = 0;
+			e_day = e_day + 1;
+			switch(e_month){
+				case 1:
+					if(e_day == 32){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 2:
+					if (e_year % 100 == 0){
+						if (e_year % 400 == 0)
+						{
+							if(e_day == 29){
+								e_day = 1
+								e_month = e_month + 1;
+							}
+						}
+						else
+						{
+							if(e_day == 28){
+								e_day = 1
+								e_month = e_month + 1;
+							}
+						}
+					}
+					else{
+						if (s_year % 4 == 0)
+						{
+							if(e_day == 29){
+								e_day = 1
+								e_month = e_month + 1;
+							}
+						}
+						else
+						{
+							if(e_day == 28){
+								e_day = 1
+								e_month = e_month + 1;
+							}
+						}
+					}
+					break;
+				case 3:
+					if(e_day == 32){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 4:
+					if(e_day == 31){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 5:
+					if(e_day == 32){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 6:
+					if(e_day == 31){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 7:
+					if(e_day == 32){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 8:
+					if(e_day == 32){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 9:
+					if(e_day == 31){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 10:
+					if(e_day == 32){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 11:
+					if(e_day == 31){
+						e_day = 1;
+						e_month = e_month + 1;
+					}
+					break;
+				case 12:
+					if(e_day == 32){
+						e_day = 1;
+						e_month = 1;
+						e_year = e_year + 1;
+					}
+					break;
+			}
+		}
+	}
+
+
+
+	var cont = "https://monitoringapi.solaredge.com/site/969566/power?startTime="+s_year+'-'+s_month+'-'+s_day+'%20'+s_hour+':'+s_min+':00&endTime='+e_year+'-'+e_month+'-'+e_day+'%20'+e_hour+':'+e_min+':00&api_key=KR647NZKDEF2ILKO8B8OG3E50UL0IMJT'; 
+	var power = 0;
+
+	const https = require('https');
+
+	https.get(cont, (resp) => {
+	  let data = '';
+
+	  // A chunk of data has been recieved.
+	  resp.on('data', (chunk) => {
+		data += chunk;
+	  });
+
+	  // The whole response has been received. Print out the result.
+	  resp.on('end', () => {
+		console.log(data);
+		if(data[125] == 'n'){
+				power = 0;
+		}
+		else{
+			let i = 125;
+			while(1){
+				if(data[i] == '.')
+				{
+					break;
+				}
+				i = i + 1;
+			}
+			for(var a=125;a<i;a++){
+				power = power*10 + parseInt(data[a]);
+			}
+		}
+		const pg = require('pg');
+		var conString = "tcp://postgres:1234@localhost/mydatabase"; 
+		const client =  new pg.Client(conString);
+		var addSql = 'INSERT INTO PV_data(Date,PV_output) VALUES($1,$2)';
+		var SQLString = s_year+'-'+s_month+'-'+s_day+' '+s_hour+':'+s_min+':00';
+		var addSqlParams = [SQLString,power];
+		client.connect(function(err) {
+			if(err) {
+			return console.error('connection failed', err);
+			}
+		});
+		console.log(addSqlParams);
+		client.query(addSql,addSqlParams,function(err,result){
+			if(err){
+				return console.error('failed',err);
+			}
+			else{
+				client.end();
+			}
+		})
+	  });
+
+	}).on("error", (err) => {
+	  console.log("Error: " + err.message);
+	});
+},1000000);
+
+
+
+
