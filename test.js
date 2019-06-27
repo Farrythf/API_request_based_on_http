c6056e54ce69ddd4bf98acafd1e0b4dc29d4b45e
Farrythf <hongfeit@usc.edu>
2019-06-26 17:32:20 -0700

    0626

--- test.js
+++ test.js
@@ -1,14 +1,24 @@
-setInterval(function(){
+setInterval(function(){						//always repeat the program
 	
-	var time = new Date();
-
+	var time = new Date();					//init time sturcture
+	
+	//get current time
 	var s_min = time.getMinutes();
 	var s_hour = time.getHours();
 	var s_year = time.getFullYear();
 	var s_month = time.getMonth() + 1;
 	var s_day = time.getDate();
 
-	s_min = parseInt(s_min / 15);
+	s_min = parseInt(s_min / 15);			//nomalize minute
+	
+	/*
+		start time handle:
+		The system will update data every 15 minutes, which means for example:
+		if it is 10:06, the system will update the data for 10:00 anytime between 10:00 and 10:15.
+		Thus, the latest solid data we can fetch is data for 9:45.
+		We rollback time for 15 minutes. Then special condition should be taken care of,such as:
+		2015-01-01, 2015-03-01...
+	*/
 	if(s_min == 0){
 		s_min = 45;
 		s_hour = s_hour - 1;
@@ -91,7 +101,13 @@ setInterval(function(){
 	var e_year = s_year;
 	var e_month = s_month;
 	var e_day = s_day;
-
+	
+	/*	
+		End time handle:
+		We always fetch one data, thus the end time is 15 minutes later than begin time.
+		Then special case should be taken care of. for example:
+		2015-12-31, 2015-2-28...
+	*/
 	if(e_min == 60){
 		e_min = 0;
 		e_hour = e_hour + 1;
@@ -123,7 +139,7 @@ setInterval(function(){
 						}
 					}
 					else{
-						if (s_year % 4 == 0)
+						if (e_year % 4 == 0)
 						{
 							if(e_day == 29){
 								e_day = 1
@@ -205,12 +221,12 @@ setInterval(function(){
 	}
 
 
-
+	// Format the sending contant
 	var cont = "https://monitoringapi.solaredge.com/site/969566/power?startTime="+s_year+'-'+s_month+'-'+s_day+'%20'+s_hour+':'+s_min+':00&endTime='+e_year+'-'+e_month+'-'+e_day+'%20'+e_hour+':'+e_min+':00&api_key=KR647NZKDEF2ILKO8B8OG3E50UL0IMJT'; 
 	var power = 0;
 
-	const https = require('https');
-
+	const https = require('https');	
+	// Http send repuest
 	https.get(cont, (resp) => {
 	  let data = '';
 
@@ -219,7 +235,7 @@ setInterval(function(){
 		data += chunk;
 	  });
 
-	  // The whole response has been received. Print out the result.
+	  // The whole response has been received. Print out the result and update it to database.
 	  resp.on('end', () => {
 		console.log(data);
 		if(data[125] == 'n'){
@@ -238,9 +254,13 @@ setInterval(function(){
 				power = power*10 + parseInt(data[a]);
 			}
 		}
+		
+		// Connect to postgreSQL
 		const pg = require('pg');
 		var conString = "tcp://postgres:1234@localhost/mydatabase"; 
 		const client =  new pg.Client(conString);
+		
+		// Send query
 		var addSql = 'INSERT INTO PV_data(Date,PV_output) VALUES($1,$2)';
 		var SQLString = s_year+'-'+s_month+'-'+s_day+' '+s_hour+':'+s_min+':00';
 		var addSqlParams = [SQLString,power];
@@ -263,7 +283,7 @@ setInterval(function(){
 	}).on("error", (err) => {
 	  console.log("Error: " + err.message);
 	});
-},1000000);
+},1000000);	// Repeat every 1000000 ms
 
 
 
